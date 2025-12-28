e2bfe0232d1b64c561200508ca86d8473709b7d3
ItsForJax <dadelos.ddls@gmail.com>
2025-12-29 00:55:26 +0800

    test: debug

--- frontend/src/pages/UploadPage.js
+++ frontend/src/pages/UploadPage.js
@@ -6,6 +6,7 @@ import SuccessModal from '../components/SuccessModal';
 import ErrorModal from '../components/ErrorModal';
 
 const API_URL = process.env.REACT_APP_API_URL || '';
+console.warn(API_URL);
 
 function UploadPage() {
   const [stats, setStats] = useState({
